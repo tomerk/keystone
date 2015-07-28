5e931ca768801ef0b75a339b1d599652638fa52b
Tomer Kaftan <tomer.kaftan@gmail.com>
2015-07-27 17:26:44 -0700

    actually set num classes....

--- src/main/scala/nodes/learning/LogisticRegressionModel.scala
+++ src/main/scala/nodes/learning/LogisticRegressionModel.scala
@@ -54,19 +54,8 @@ case class LogisticRegressionLBFGSEstimator[T <: Vector[Double] : ClassTag](numC
     extends LabelEstimator[T, Double, Int] {
   override def fit(in: RDD[T], labels: RDD[Int]): LogisticRegressionModel[T] = {
     val labeledPoints = labels.zip(in).map(x => LabeledPoint(x._1, breezeVectorToMLlib(x._2)))
-    val trainer = new LogisticRegressionWithLBFGS()
+    val trainer = new LogisticRegressionWithLBFGS().setNumClasses(numClasses)
     trainer.setValidateData(false).optimizer.setConvergenceTol(convergenceTol)
-    val model = trainer.run(labeledPoints)
-
-    new LogisticRegressionModel(model)
-  }
-}
-
-case class MultiNomialLogisticRegressionLBFGSEstimator[T <: Vector[Double] : ClassTag](numClasses: Int = 2)
-    extends LabelEstimator[T, Double, Int] {
-  override def fit(in: RDD[T], labels: RDD[Int]): LogisticRegressionModel[T] = {
-    val labeledPoints = labels.zip(in).map(x => LabeledPoint(x._1, breezeVectorToMLlib(x._2)))
-    val trainer = new LogisticRegressionWithLBFGS().setValidateData(false).setNumClasses(numClasses)
     val model = trainer.run(labeledPoints)
 
     new LogisticRegressionModel(model)
