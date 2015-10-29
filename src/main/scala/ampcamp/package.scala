f1b020e0c0d910ae0096cd0fc7d1f42ccdb7aaef
Tomer Kaftan <tomer.kaftan@gmail.com>
2015-10-29 11:53:16 -0700

    progress on ampcamp interactivity

--- src/main/scala/ampcamp/package.scala
+++ src/main/scala/ampcamp/package.scala
@@ -1,5 +1,7 @@
+import breeze.linalg.DenseVector
 import evaluation.MulticlassClassifierEvaluator
-import loaders.{NewsgroupsDataLoader, LabeledData}
+import loaders.{CsvDataLoader, NewsgroupsDataLoader, LabeledData}
+import nodes.util.{MaxClassifier, ClassLabelIndicatorsFromIntLabels}
 import org.apache.spark.SparkContext
 import org.apache.spark.rdd.RDD
 import workflow.Pipeline
@@ -30,6 +32,30 @@ package object ampcamp {
     System.out.println(s"Total Accuracy: ${fmt.format(testEval.totalAccuracy)}")
     System.out.println(testEval.pprintConfusionMatrix(newsgroupsClasses))
   }
+
+  private val mnistNumPartitions = 10
+  private val mnistDataMap = mutable.Map.empty[String, LabeledData[Int, DenseVector[Double]]]
+  def loadMnistData(sc: SparkContext, dataDir: String): (RDD[DenseVector[Double]], RDD[DenseVector[Double]]) = {
+    val trainLabeledData = mnistDataMap.getOrElseUpdate(dataDir, {
+      LabeledData(
+        CsvDataLoader(sc, dataDir, mnistNumPartitions)
+        // The pipeline expects 0-indexed class labels, but the labels in the file are 1-indexed
+        .map(x => (x(0).toInt - 1, x(1 until x.length)))
+            .cache())
+    })
+    (trainLabeledData.data, ClassLabelIndicatorsFromIntLabels(mnistClasses.length).apply(trainLabeledData.labels))
+  }
+
+  val mnistClasses = Array("0", "1", "2", "3", "4", "5", "6", "7", "8", "9")
+  def evalMnistPipeline(pipeline: Pipeline[DenseVector[Double], Int], sc: SparkContext, dataDir: String): Unit = {
+    val (testData, testLabels) = loadMnistData(sc, dataDir)
+    val testResults = pipeline(testData)
+    val testEval = MulticlassClassifierEvaluator(testResults, MaxClassifier(testLabels), mnistClasses.length)
+
+    val fmt = "%2.3f"
+    System.out.println(s"Total Accuracy: ${fmt.format(testEval.totalAccuracy)}")
+    System.out.println(testEval.pprintConfusionMatrix(mnistClasses))
+  }
 }
 
 /*
@@ -55,5 +81,33 @@ val pipeline = {
 }
 
 evalNewsgroupsPipeline(pipeline, sc, "/Users/tomerk11/Downloads/20news-bydate/20news-bydate-test")
+
+ */
+
+/*
+import ampcamp._
+import nodes.learning._
+import nodes.util._
+import workflow._
+import breeze.linalg.DenseVector
+
+val (trainData, trainLabels) = loadMnistData(sc, "/Users/tomerk11/Desktop/mnist/train-mnist-dense-with-labels.data")
+
+val featurizer = Transformer[DenseVector[Double], DenseVector[Double]](identity)
+//val featurizer = PaddedFFT() andThen LinearRectifier(0.0)
+//val featurizer = RandomSignNode(mnistImageSize) andThen PaddedFFT() andThen LinearRectifier(0.0)
+/*val featurizer = Pipeline.gather {
+  Seq.fill(conf.numFFTs) {
+    RandomSignNode(mnistImageSize) andThen PaddedFFT() andThen LinearRectifier(0.0)
+  }
+} andThen VectorCombiner()*/
+
+val pipeline = {
+    featurizer andThen
+    (LinearMapEstimator(lambda = Some(1.0)), trainData, trainLabels) andThen
+    MaxClassifier
+}
+
+evalMnistPipeline(pipeline, sc, "/Users/tomerk11/Desktop/mnist/test-mnist-dense-with-labels.data")
 
  */
