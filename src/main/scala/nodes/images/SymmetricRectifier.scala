4e503a38477ae2806fd2080452f08a4c7e8d9450
Evan Sparks <sparks@cs.berkeley.edu>
2016-04-15 18:24:35 -0700

    Swapping loop ordering in rectifier.

--- src/main/scala/nodes/images/SymmetricRectifier.scala
+++ src/main/scala/nodes/images/SymmetricRectifier.scala
@@ -1,30 +1,30 @@
 package nodes.images
 
 import pipelines._
-import utils.{ChannelMajorArrayVectorizedImage, Image}
+import utils.{RowMajorArrayVectorizedImage, ChannelMajorArrayVectorizedImage, Image}
 import workflow.Transformer
 
 case class SymmetricRectifier(maxVal: Double = 0.0, alpha: Double = 0.0)
   extends Transformer[Image, Image] {
 
   def apply(img: Image): Image = {
-    val res = ChannelMajorArrayVectorizedImage(
+    val res = RowMajorArrayVectorizedImage(
       new Array[Double](img.metadata.xDim * img.metadata.yDim * img.metadata.numChannels * 2),
       img.metadata.copy(numChannels = img.metadata.numChannels * 2))
 
     var x, y, c = 0
-    while (x < img.metadata.xDim) {
+    while (c < img.metadata.numChannels) {
       y = 0
       while (y < img.metadata.yDim) {
-        c = 0
-        while (c < img.metadata.numChannels) {
+        x = 0
+        while (x < img.metadata.xDim) {
           res.put(x, y, c, math.max(maxVal, img.get(x, y, c) - alpha))
           res.put(x, y, c + img.metadata.numChannels, math.max(maxVal, -img.get(x, y, c) - alpha))
-          c += 1
+          x += 1
         }
         y += 1
       }
-      x += 1
+      c += 1
     }
 
     res
